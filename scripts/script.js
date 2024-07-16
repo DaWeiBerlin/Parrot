7dc90e38534517c8ead8aafb667686bf9b3b933b
DaWeiBerlin <davidburkert@outlook.com>
2024-07-16 15:49:07 +0200

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 //global speichern
 //notizen anzeigen lassen
 let notes = ["Kirsche","Erdnuss","spielen"]
-let trash = []
+let trashe = []
 function renderNotes(){
     content.innerHTML = ""
     for(let i = 0; i < notes.length;i++){
@@ -9,10 +9,10 @@ function renderNotes(){
         <div class="note">${notes[i]} <a onclick="NotizLöschen(${i})">X</a></div>
         `;
     }
-    trash.innerHTML = ""
-    for(let j = 0; j < trash.length;j++){
-        trash.innerHTML += /*html*/`
-        <div class="note">${trash[j]} <a onclick="Löschen(${j})">X</a></div>
+    document.getElementById("trash").innerHTML = ""
+    for(let j = 0; j < trashe.length;j++){
+        document.getElementById("trash").innerHTML += /*html*/`
+        <div class="note">${trashe[j]} <a onclick="Löschen(${j})">X</a></div>
         `;
     }    
 }
@@ -26,15 +26,16 @@ function NotizHinzufügen(){
 
 function NotizLöschen(i){
     console.log(notes[i])
-    trash.push(notes[i])
-    console.log(trash)
+    trashe.push(notes[i])
+    console.log(trashe)
     notes.splice(i,1)
 
     renderNotes()
 }
 
 function Löschen(i){
-    trash.splice(i,1)
+    trashe.splice(i,1)
+    renderNotes()
 }
 //notizen hinzufügen
 //notizen löschen
